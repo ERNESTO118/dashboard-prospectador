5c21b43fb70ba9fb31ac82c51864f9e8dbd1cfb0
ERNESTO118 <moises150167@gmail.com>
2025-08-29 12:44:44 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("¡Mi Dashboard Funciona en su Nueva Casa!")
+st.write("¡Hemos vencido a Replit!")
+
+if st.button("¡Celébralo!"):
+    st.success("¡Lo logramos!")
+    st.balloons()
